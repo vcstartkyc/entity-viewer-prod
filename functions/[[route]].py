02bd644c77122119ae405006dcf1773bee26257f
vcstartkyc <vc+github@startkyc.com>
2025-03-02 14:49:11 +0100

    Add files via upload

--- functions/[[route]].py
+++ functions/[[route]].py
@@ -10,13 +10,15 @@ from app import app as flask_app
 def onRequest(context):
     """Handle the incoming request using the Flask app."""
     with flask_app.test_client() as client:
+        # Forward the request to Flask
         response = client.open(
-            path=context.request.path,
+            path=context.request.path or '/',  # Default to / if path is empty
             method=context.request.method,
             headers=dict(context.request.headers),
             data=context.request.get_data()
         )
         
+        # Convert Flask response to Pages response
         return Response(
             response.get_data(),
             status=response.status_code,
